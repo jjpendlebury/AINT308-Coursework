2e4d54fba0234a75592fb724cdcbc4d3653ffe17
Jack Pendlebury <jack.j.pendlebury@students.plymouth.ac.uk>
2022-01-31 17:09:39 +0000

    Task 2 Complete

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -6,11 +6,14 @@
 using namespace std;
 using namespace cv;
 
+#define PI 3.14159265
+#define halfPI 1.570796325
+
 int main()
 {
-
-    VideoCapture InputStream("../Task2/Video.mp4"); //Load in the video as an input stream
-    const Point Pivot(592,52);                                      //Pivot position in the video
+    namedWindow("Task 2 - 10613591", WINDOW_NORMAL);
+    VideoCapture InputStream("../Task2/Video.mp4");                     //Load in the video as an input stream
+    const Point Pivot(592,52);                                          //Pivot position in the video
 
     //Open output file for angle data
     ofstream DataFile;
@@ -34,22 +37,47 @@ int main()
         //======================================================Your code goes here====================================================
         //this code will run for each frame of the video. your task is to find the location of the swinging green target, and to find
         //its angle to the pivot. These angles will be saved to a .csv file where they can be plotted in Excel.
+        int HueLower=70;
+        int HueUpper=80;
+        int SatLower=30;
+        int SatUpper=255;
+        int ValLower=30;
+        int ValUpper=255;
+
+        Mat FrameHSV;
+        cvtColor(Frame,FrameHSV,COLOR_BGR2HSV); //convert to HSV
+        Mat FrameFiltered;
+        Vec3b LowerBound(HueLower, SatLower, ValLower);
+        Vec3b UpperBound(HueUpper, SatUpper, ValUpper);
+        inRange(FrameHSV, LowerBound, UpperBound, FrameFiltered);
+
+        Moments m = moments(FrameFiltered, true);
+        Point p(m.m10/m.m00, m.m01/m.m00);
+        Point p_left(m.m10/m.m00, (m.m01/m.m00)-50);
+        Point p_right(m.m10/m.m00, (m.m01/m.m00)+50);
+        Point p_up((m.m10/m.m00)+50, (m.m01/m.m00));
+        Point p_down((m.m10/m.m00)-50, (m.m01/m.m00));
+        circle(Frame, p, 20, Scalar(255,0,0), 3);
+        circle(Frame, Pivot, 10, Scalar(255,0,0), -1);
+        line(Frame, p,Pivot, Scalar(255,0,0), 2);
+        line(Frame, p_left, p_right,Scalar(255,0,0), 2);
+        line(Frame, p_up, p_down,Scalar(255,0,0), 2);
+
+        double angle = (atan2(p.x,p.y))-M_PI_4;
 
 
 
+        cout << angle <<endl;
+        DataFile << angle <<endl;
+        Mat3b combined,FilterRGB;
+        cvtColor(FrameFiltered,FilterRGB,COLOR_GRAY2BGR);
 
-
-
-
-
-
-
-
-
+        hconcat(Frame,FilterRGB,combined);
         //==============================================================================================================================
 
         //display the frame
-        imshow("Video", Frame);
+        imshow("Task 2 - 10613591", combined);
+        //imshow("Task 2 - 10613591", Frame);
         waitKey(10);
     }
 
